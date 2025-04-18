e18ec254e2aba0863a83dfcbfcce569a8c6d1dc9
Tino Klijn <tino@dotnet2web.nl>
2025-04-18 09:48:56 +0200

    Exclude this exception from codecoverage. We do use it in a test within the CsvCoreWriter

--- CsvCore/Exceptions/FileWritingException.cs
+++ CsvCore/Exceptions/FileWritingException.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace CsvCore.Exceptions;
 
+[ExcludeFromCodeCoverage]
 public class FileWritingException : Exception
 {
     public FileWritingException() { }
