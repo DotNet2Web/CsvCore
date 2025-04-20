055cb054073d61c9bf29ccee19cdf3b6b031700e
Tino Klijn <tino@dotnet2web.nl>
2025-04-20 15:24:06 +0200

    Defense if a model is zerobased

---
+++ CsvCore.Specs/Models/ZeroBaseNotMatchingPersonModel.cs
@@ -0,0 +1,19 @@
+using System;
+using CsvCore.Attributes;
+
+namespace CsvCore.Specs.Models;
+
+public class ZeroBaseNotMatchingPersonModel
+{
+    [Header(1)]
+    public string Name { get; set; }
+
+    [Header]
+    public string Surname { get; set; }
+
+    [Header(2)]
+    public DateOnly BirthDate { get; set; }
+
+    [Header(3)]
+    public string Email { get; set; }
+}
